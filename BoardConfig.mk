803a0899872976481277eeeb77a35e3acc5fa3cc
StarkDroid <gamerprince.exp@gmail.com>
2016-02-14 14:38:19 +0100

    Enable optimizations

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,16 +25,19 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8610
 BOARD_CUSTOM_BOOTIMG_MK := device/motorola/condor/mkbootimg.mk
 
 #Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3/bin
 
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 5.2
+TARGET_GCC_VERSION_EXP := 5.3
 
 # Optimization
-GRAPHITE_OPTS := true
-ENABLE_GCCONLY := true
+CLANG_O3 := true
+STRICT_ALIASING := false
+KRAIT_TUNINGS := false
+GRAPHITE_OPTS := false
+ENABLE_GCCONLY := false
 
 # Storage & partiiton
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
